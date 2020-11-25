66af8faf8a77e327a11bb46c31a97a4df557c5f3
LIJOMLOYID <lijomloyid@gmail.com>
2020-11-25 15:22:08 +0530

    Added delete functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,7 +6,11 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
-  handleDelete = (movie) => {};
+  handleDelete = (movie) => {
+    console.log(movie);
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
 
   render() {
     return (
@@ -17,15 +21,24 @@ class Movies extends Component {
             <th>Genere</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
