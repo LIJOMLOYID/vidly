1b378e8de4efb87b422fa76bf18afeecf5f2f541
LIJOMLOYID <lijomloyid@gmail.com>
2021-01-28 14:55:10 +0530

    Completed like functionality, Refactored Like Component to functional component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -3,18 +3,16 @@ import React, { Component } from "react";
 //Input: liked: boolean
 //Output: onClick
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        className={classes}
-      />
-    );
-  }
-}
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+    />
+  );
+};
 
 export default Like;
